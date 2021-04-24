18a3911d510875ff3174b5e52cd46e1b201d0243
Daniel Kütt <danielkutt@hey.com>
2021-04-24 23:36:31 +0300

    Test bme280 script

--- sensor-service/app.py
+++ sensor-service/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 
+import bme280
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    temperature, pressure, humidity = bme280.readBME280All()
+    return f'Temperature: {temperature}; Pressure: {pressure}; Humidity: {humidity}'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
