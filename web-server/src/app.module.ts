e6ad05225ddae75dc4d6446892ff345461db998a
Daniel Kütt <danielkutt@hey.com>
2021-06-19 23:03:17 +0300

    Add fast and slow entry monitoring

--- web-server/src/app.module.ts
+++ web-server/src/app.module.ts
@@ -4,11 +4,11 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
-import { TasksService } from './tasks.service';
+import { RasperryMonitoringService } from './rasperryMonitoring.service';
 
 @Module({
   imports: [ScheduleModule.forRoot(), ConfigModule.forRoot(), HttpModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, TasksService],
+  providers: [AppService, PrismaService, RasperryMonitoringService],
 })
 export class AppModule {}
---
+++ web-server/src/rasperryMonitoring.service.ts
@@ -0,0 +1,115 @@
+import { HttpService, Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { PrismaService } from './prisma.service';
+import { ConfigService } from '@nestjs/config';
+
+interface RasperryData {
+  fast: {
+    windDirection: number;
+    stateful: {
+      windSpeed: number;
+    };
+  };
+  slow?: {
+    bme680: {
+      humidity: number;
+      pressure: number;
+      temperature: number;
+      gas: number;
+    };
+    stateful: {
+      rain: number;
+    };
+  };
+  timeStamp: string;
+}
+
+@Injectable()
+export class RasperryMonitoringService {
+  constructor(
+    private prisma: PrismaService,
+    private httpService: HttpService,
+    private configService: ConfigService,
+  ) {
+    this.weatherStationUrl =
+      this.configService.get<string>('WEATHERSTATION_URL');
+
+    // Do an initial measurement to reset the state
+    this.httpService.get<Required<RasperryData>>(
+      `${this.weatherStationUrl}/all`,
+    );
+  }
+
+  private readonly weatherStationUrl: string;
+  private counter = 0;
+
+  private async getAllEntries() {
+    const allEntries = await this.httpService
+      .get<Required<RasperryData>>(`${this.weatherStationUrl}/all`)
+      .toPromise();
+
+    const {
+      fast: {
+        windDirection,
+        stateful: { windSpeed },
+      },
+      slow: {
+        bme680: { humidity, pressure, temperature, gas },
+        stateful: { rain },
+      },
+    } = allEntries.data;
+
+    await this.prisma.raspberryFastEntry.create({
+      data: {
+        windDirection,
+        windSpeed,
+      },
+    });
+
+    await this.prisma.raspberrySlowEntry.create({
+      data: {
+        bme680Humidity: humidity,
+        bme680Pressure: pressure,
+        bme680Temperature: temperature,
+        bme680Gas: gas,
+        rainfall: rain,
+      },
+    });
+  }
+
+  private async getFastEntries() {
+    const fastEntries = await this.httpService
+      .get<RasperryData>(`${this.weatherStationUrl}/fast`)
+      .toPromise();
+
+    const {
+      windDirection,
+      stateful: { windSpeed },
+    } = fastEntries.data.fast;
+
+    await this.prisma.raspberryFastEntry.create({
+      data: {
+        windDirection,
+        windSpeed,
+      },
+    });
+  }
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async measureRaspberry() {
+    try {
+      if (this.counter === 5) {
+        await this.getAllEntries();
+      } else {
+        await this.getFastEntries();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    this.counter++;
+    if (this.counter >= 6) {
+      this.counter = 0;
+    }
+  }
+}
