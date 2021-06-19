5ee511c20e387d2c4a1f16e325376da113f814e1
Daniel Kütt <danielkutt@hey.com>
2021-06-19 23:00:25 +0300

    Change windspeed sensor to speed

--- sensor-service/windspeed_sensor.py
+++ sensor-service/windspeed_sensor.py
@@ -1,11 +1,12 @@
 from gpiozero import Button
+import time
 import math
-
 
 wind_sensor = Button(5)
 count = 0
 radius_cm = 9.0
 anemometer_factor = 1.18
+last_measurement = time.time()
 
 
 def spin():
@@ -13,20 +14,21 @@ def spin():
     count = count + 1
 
 
-def reset_spins():
-    global count
-    count = 0
-
-
 def get_reading():
     global count
+    global last_measurement
+
     circumference_cm = (2 * math.pi) * radius_cm
     rotations = count / 2.0
-    dist_cm = circumference_cm * rotations
-    result = dist_cm
+    dist_cm = circumference_cm * rotations * anemometer_factor
+    dist_meters = dist_cm * 0.01
+    count = 0
 
-    reset_spins()
-    return result * anemometer_factor
+    current_time = time.time()
+    time_diff = last_measurement - current_time
+    last_measurement = current_time
+
+    return dist_meters / time_diff
 
 
 wind_sensor.when_pressed = spin
