407c13acd5168efb9fd948b54f805726d9d20681
Daniel Kütt <danielkutt@hey.com>
2021-06-19 19:28:24 +0300

    Add cron job base

--- web-server/src/app.module.ts
+++ web-server/src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot(), ConfigModule.forRoot(), HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, TasksService],
 })
 export class AppModule {}
