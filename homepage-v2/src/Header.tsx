77468cfcaaaaed37579c02f23f665aab035966ab
Daniel Kütt <danielkutt@hey.com>
2021-07-04 13:59:38 +0300

    Add moon phase

--- homepage-v2/src/Header.tsx
+++ homepage-v2/src/Header.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { getSunrise, getSunset } from 'sunrise-sunset-js';
 // @ts-ignore
-import { Moon, Hemisphere } from 'lunarphase-js/dist/lunarphase-js.esm';
+import { Moon, Hemisphere, LunarPhase } from 'lunarphase-js/dist/lunarphase-js.esm';
 
 const coordinates = [57.9547156, 27.6021553];
 const dateFormatter = new Intl.DateTimeFormat('et-EE', {
   month: 'long',
   day: 'numeric',
 });
+
 const numberPadder = (inputNumber: number) =>
   inputNumber < 10 ? `0${inputNumber}` : String(inputNumber);
+
+const lunarPhaseTranslations = (lunarPhase: string) => {
+  switch (lunarPhase) {
+    case LunarPhase.NEW:
+      return 'Kuu loomine';
+    case LunarPhase.WANING_CRESCENT:
+      return 'Noorkuu';
+    case LunarPhase.FIRST_QUARTER:
+      return 'Poolkuu esimene veerand';
+    case LunarPhase.WANING_GIBBOUS:
+      return 'Kasvav kuu';
+    case LunarPhase.FULL:
+      return 'Täiskuu';
+    case LunarPhase.WAXING_GIBBOUS:
+      return 'Kahanev kuu';
+    case LunarPhase.LAST_QUARTER:
+      return 'Poolkuu viimane veerand';
+    case LunarPhase.WAXING_CRESCENT:
+      return 'Vanakuu';
+  }
+};
 
 const Header = () => {
   const sunrise = getSunrise(coordinates[0], coordinates[1]);
@@ -19,15 +41,21 @@ const Header = () => {
       <div className="container mx-auto">
         <h1 className="text-3xl font-medium">Värska</h1>
         <p>{dateFormatter.format(Date.now())}</p>
-        <p>
-          Päikesetõus: {numberPadder(sunrise.getHours())}:
+
+        <p className="flex items-center text-lg">
+          <span className="text-4xl mr-2">🌅</span>{' '}
+          {numberPadder(sunrise.getHours())}:
           {numberPadder(sunrise.getMinutes())}
         </p>
-        <p>
-          Päikeseloojang: {numberPadder(sunset.getHours())}:
-          {numberPadder(sunset.getMinutes())}
+        <p className="flex items-center text-lg">
+          <span className="text-4xl mr-2">🌇</span>{' '}
+          {numberPadder(sunset.getHours())}:{numberPadder(sunset.getMinutes())}
         </p>
-        <p>{Moon.lunarPhaseEmoji(new Date() , Hemisphere.NORTHERN)}</p>
+        <p className="flex items-center">
+          <span className="text-4xl mr-2">
+            {Moon.lunarPhaseEmoji(new Date(), Hemisphere.NORTHERN)}
+          </span> {lunarPhaseTranslations(Moon.lunarPhase())}
+        </p>
       </div>
     </header>
   );
