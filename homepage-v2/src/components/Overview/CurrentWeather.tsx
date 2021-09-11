abf65efb237a5a705144a042d139db0705c12c54
Daniel Kütt <danielkutt@hey.com>
2021-09-11 16:24:45 +0300

    Add average windspeed

--- homepage-v2/src/components/Overview/CurrentWeather.tsx
+++ homepage-v2/src/components/Overview/CurrentWeather.tsx
@@ -13,9 +13,9 @@ const CurrentWeather = ({ data: [fastData, slowData] }: Props) => {
   }
 
   const data = [...slowData].reverse();
+  const windData = [...fastData].reverse();
 
-  const { bme680Temperature, bme680Pressure, bme680Humidity } =
-    data[0];
+  const { bme680Temperature, bme680Pressure, bme680Humidity } = data[0];
   const temperature = roundWithDecimalPlaces(bme680Temperature, 1);
   const pressure = roundWithDecimalPlaces(bme680Pressure, 1);
   const humidity = roundWithDecimalPlaces(bme680Humidity, 1);
@@ -25,7 +25,13 @@ const CurrentWeather = ({ data: [fastData, slowData] }: Props) => {
     const datum = data[i];
     rain = rain + datum.rainfall;
   }
-  rain = Math.round(rain * 10) / 10
+  rain = Math.round(rain * 10) / 10;
+
+  let windSpeed = 0;
+  for (let i = 0; i < Math.min(windData.length, 10); i++) {
+    windSpeed += windData[i].windSpeed * 0.4;
+  }
+  windSpeed = Math.round(windSpeed * 10) / 10;
 
   return (
     <div className="flex">
@@ -33,6 +39,7 @@ const CurrentWeather = ({ data: [fastData, slowData] }: Props) => {
         <p>Temperatuur: {temperature} °C</p>
         <p>Õhurõhk: {pressure} hPa</p>
         <p>Õhuniiskus: {humidity}%</p>
+        <p>Tuule kiirus: {windSpeed} m/s</p>
         <p>24h sademed: {rain} mm</p>
       </Card>
     </div>
